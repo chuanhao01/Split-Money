3e161d6192a892c702f53a8996e244200d515c2b
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2024-06-14 16:23:04 +0800

    [feat] got db working with accounts

--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,23 @@ import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from 
 import type { NextAuthOptions } from 'next-auth';
 import { getServerSession } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import PostgresAdapter from '@auth/pg-adapter';
+import { Pool } from 'pg';
+import { Adapter } from 'next-auth/adapters';
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
 export const config = {
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: PostgresAdapter(pool) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.CLIENT_ID as string,
